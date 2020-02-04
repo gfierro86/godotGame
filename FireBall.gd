8d6681da9f5cb1dd7d1776b6592390dd42ae83bc
gfierro <gfierro@hotels.com>
2020-02-04 09:50:53 +0100

    gameplay changes

--- FireBall.gd
+++ FireBall.gd
@@ -1,18 +1,20 @@
 extends Area2D
 
 export var FIREBALL_SPEED = 500
-var direction = Vector2(0, -1);
+var direction = Vector2(0, -1)
+var actualSpeed = FIREBALL_SPEED
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
 	
-func start_at(dir, pos):
-	print("called start_at:", dir, pos)
+func start_at(dir, pos, shipSpeed):
 	set_rotation(dir)
 	set_position(pos)
 	direction = direction.rotated(dir) 
+	actualSpeed += shipSpeed
 
 func _physics_process(delta):
-	var motion = direction * delta * FIREBALL_SPEED;
+	print("Fireball velocity:", actualSpeed)
+	var motion = direction * (delta * actualSpeed);
 	set_position(get_position() + motion)
--- Ship.gd
+++ Ship.gd
@@ -3,10 +3,10 @@ extends RigidBody2D
 
 export var FIREBALL_SCENE = preload("res://FireBall.tscn")
 export var FIRE_RATE = 3
-export var ROT_SPEED = 100
+export var ROT_SPEED = 75
 export var FW_SPEED = 10
-export var MAX_SPEED = 50
-export var BRAKE_SPEED = 15
+export var MAX_SPEED = 300
+export var BRAKE_SPEED = 2
 var fireInterval = 1000 / FIRE_RATE
 var lastFiredTime = OS.get_ticks_msec()
 var velocity = 0
@@ -22,21 +22,25 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		rotation_degrees -= ROT_SPEED * delta
 	if Input.is_action_pressed("ui_up"):
-		velocity += (FW_SPEED * delta)
+		velocity += FW_SPEED
 		if velocity > MAX_SPEED:
 			velocity = MAX_SPEED
 	else:
-		velocity -= (BRAKE_SPEED * delta)
+		velocity -= BRAKE_SPEED
 		if velocity < 0:
 			velocity = 0
-	position += velocity * Vector2(0,-1).rotated(deg2rad(rotation_degrees))
+	var motion = velocity * delta * Vector2(0,-1).rotated(deg2rad(rotation_degrees))
+	print("velocity: ",velocity)
+	set_position(get_position() + motion)
+	position.x = clamp(position.x, -1024, 1024)
+	position.y = clamp(position.y, -1024, 1024)
 	
 func _shoot():
 	var firepoint = get_node("Firepoint")
 	var fireball = FIREBALL_SCENE.instance()
 	var rotRadiant = deg2rad(rotation_degrees)
 	var actualFirepointPos = position + firepoint.position.rotated(rotRadiant)
-	fireball.start_at(rotRadiant, actualFirepointPos)
+	fireball.start_at(rotRadiant, actualFirepointPos, velocity)
 	get_parent().add_child(fireball)
 	
 	
